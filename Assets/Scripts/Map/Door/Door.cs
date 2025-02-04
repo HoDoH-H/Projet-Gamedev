09f0137f552f41c90f63e0d947ca8ca7e7722d3c
HoDoH-H <144995071+HoDoH-H@users.noreply.github.com>
2025-02-04 12:19:01 +0100

    Finished Door logics and started Broken Door state

--- Assets/Scripts/Map/Door/Door.cs
+++ Assets/Scripts/Map/Door/Door.cs
@@ -2,30 +2,48 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    [SerializeField] private Transform door;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private BoxCollider box;
+    [SerializeField] private MeshCollider col;
     [SerializeField] private float maxRotationToTheRight = 90f;
     [SerializeField] private float maxRotationToTheLeft = -90f;
-    [SerializeField] private Vector3 currentRotation;
+    [SerializeField] private float maxRotationSpeedToAutoClose = 0.6f;
+
+    [Header("Debug")]
+    [SerializeField] private bool doBreak = false;
 
     private void Update()
     {
-        currentRotation = Quaternion.Euler(door.localEulerAngles).eulerAngles;
         ControlRotation();
+        Break(doBreak);
     }
 
     void ControlRotation()
     {
-        if (door.localEulerAngles.y > 0 + maxRotationToTheRight && door.localEulerAngles.y < 0 + maxRotationToTheRight + 5)
+        if (transform.localEulerAngles.y > maxRotationToTheRight && transform.localEulerAngles.y < 300 + maxRotationToTheLeft)
         {
-            Debug.Log("Max angle reached");
-            door.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-            door.Rotate(new Vector3(0, -1, 0), Space.Self);
+            rb.angularVelocity = Vector3.zero;
+            transform.Rotate(new Vector3(0, -1, 0), Space.Self);
         }
-        else if (door.localEulerAngles.y > 360 - maxRotationToTheLeft)
+        else if(transform.localEulerAngles.y < 360 + maxRotationToTheLeft && transform.localEulerAngles.y > maxRotationToTheRight + 30)
         {
-            Debug.Log("Min angle reached");
-            door.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-            door.Rotate(new Vector3(0, 1, 0), Space.Self);
+            rb.angularVelocity = Vector3.zero;
+            transform.Rotate(new Vector3(0, 1, 0), Space.Self);
+        }
+        else if(transform.localEulerAngles.y < 2 && Mathf.Abs(transform.GetComponent<Rigidbody>().angularVelocity.y) < maxRotationSpeedToAutoClose || transform.localEulerAngles.y > 358 && Mathf.Abs(transform.GetComponent<Rigidbody>().angularVelocity.y) < maxRotationSpeedToAutoClose)
+        {
+            transform.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        }
+    }
+
+    public void Break(bool doBreak = true)
+    {
+        if(doBreak && transform.parent != null)
+        {
+            rb.constraints = RigidbodyConstraints.None;
+            transform.parent = null;
+            rb.automaticCenterOfMass = true;
+            col.excludeLayers = 0;
         }
     }
 
